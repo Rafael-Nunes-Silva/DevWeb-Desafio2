9a979a5cab634eed8fde179655764b49cfbac34e
Rafael Silva <76886825+Rafael-Nunes-Silva@users.noreply.github.com>
2023-03-28 08:51:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="./static", template_folder="./template")
+
+@app.route("/")
+def renderIndex():
+    return render_template("index.html")
+
+@app.route("/contato")
+def renderContato():
+    return render_template("contato.html")
+
+@app.route("/quemsomos")
+def renderQuemSomos():
+    return render_template("quemsomos.html")
